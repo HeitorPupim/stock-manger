c0d6984a4b7d3fb26115805ab76dc751b9f1598a
HeitorPupim <heitorpupimtoledo@gmail.com>
2026-01-19 10:25:15 -0300

    feat: Implement a basic login page, add `tailwindcss-animate` dependency, update home page button text, and configure VS Code settings.

--- stock-manager/src/app/page.tsx
+++ stock-manager/src/app/page.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex flex-wrap items-center gap-2 md:flex-row">
-      <Button variant="outline">Button</Button>
+      <Button variant="outline">Home Page</Button>
       <Button variant="outline" size="icon" aria-label="Submit">
         <ArrowUpIcon />
       </Button>
