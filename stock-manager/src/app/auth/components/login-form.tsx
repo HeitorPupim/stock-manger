acfc99cd2768a721ae4fc74b0b2ae8877c276917
HeitorPupim <heitorpupimtoledo@gmail.com>
2026-01-19 15:08:46 -0300

    feat: Extract login form into a dedicated component with client-side validation.

---
+++ stock-manager/src/app/auth/components/login-form.tsx
@@ -0,0 +1,95 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const loginSchema = z.object({
+  name: z.string().trim().min(3, "Nome deve ter pelo menos 3 caracteres"),
+  email: z.string().trim().email("Email inválido"),
+  password: z.string().trim().min(8, "Senha deve ter pelo menos 8 caracteres"),
+});
+
+const LoginForm = () => {
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof loginSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  
+  return (
+    <Card>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>
+              Faça login para acessar o sistema
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4 space-y-2">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Seu email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Senha" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
+  );
+};
+
+export default LoginForm;
--- stock-manager/src/app/auth/page.tsx
+++ stock-manager/src/app/auth/page.tsx
@@ -11,14 +11,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+import LoginForm from "./components/login-form";
 import SignUpForm from "./components/sign-up-form";
 
 
-const AuthenticationPage = () => {
 
+const AuthenticationPage = () => {
 
   return (
     <div className="flex h-screen w-screen items-center justify-center">
@@ -29,25 +29,7 @@ const AuthenticationPage = () => {
             <TabsTrigger value="register">Criar Conta</TabsTrigger>
           </TabsList>
           <TabsContent value="login">
-            <Card>
-              <CardHeader>
-                <CardTitle>Login</CardTitle>
-                <CardDescription>Faça login na sua conta</CardDescription>
-              </CardHeader>
-              <CardContent className="grid gap-6">
-                <div className="grid gap-3">
-                  <Label htmlFor="tabs-demo-name">Email</Label>
-                  <Input id="tabs-demo-name" defaultValue="usuario@gmail.com" />
-                </div>
-                <div className="grid gap-3">
-                  <Label htmlFor="tabs-demo-username">Senha</Label>
-                  <Input id="tabs-demo-username" defaultValue="123456" />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button>Login</Button>
-              </CardFooter>
-            </Card>
+            <LoginForm />
           </TabsContent>
           <TabsContent value="register">
             <SignUpForm />
